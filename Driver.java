7af7c521ba40572c015c8290c35ea81a9302b9a6
BrianDahi <brian_dahi@yahoo.com>
2019-09-18 11:52:12 -0500

    first commit

---
+++ Driver.java
@@ -0,0 +1,39 @@
+/**
+ * @author Mohammad Mukhtaruzzaman
+ * @version 2019-08-29
+*/
+import java.io.IOException;
+
+
+public class Driver 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		//HammingDist something = new HammingDist("Mesonet.txt");
+		
+		HammingDist hammDist1 = new HammingDist("PORT", "SALL");
+		
+		HammingDist hammDist2 = new HammingDist("BOIS", "WEBR");
+		System.out.println("Hello");
+			
+		
+		try{
+			hammDist1.read("Mesonet.txt");
+		}
+		catch(Exception e) {
+			System.out.println("wrong");
+		}
+		try{
+			hammDist2.read("Mesonet.txt");
+		}
+		catch(Exception e) {
+			System.out.println("wrong");
+		}
+		System.out.println("Hello");
+		
+		
+		System.out.println(hammDist1.toString());
+		System.out.println("");
+		System.out.println(hammDist2.toString());
+	}
+}
